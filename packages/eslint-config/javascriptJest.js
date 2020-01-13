088a74e755ef60172b842a56029cbcda2de0782b
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-13 09:56:08 +0100

    add jest rules

---
+++ packages/eslint-config/javascriptJest.js
@@ -0,0 +1,22 @@
+// https://github.com/jest-community/eslint-plugin-jest
+const eslintConfigForJest = {
+  "plugins": ["jest"],
+  "extends": ["plugin:jest/style"],
+  rules: {
+    // additional rules
+    "jest/consistent-test-it": "error",
+    "jest/no-duplicate-hooks": "error",
+    "jest/no-expect-resolves": "error",
+    "jest/no-test-return-statement": "error",
+    "jest/no-truthy-falsy": "error",
+    "jest/prefer-hooks-on-top": "error",
+    "jest/prefer-spy-on": "error",
+    "jest/prefer-strict-equal": "error",
+    "jest/prefer-todo": "error",
+    "jest/require-top-level-describe": "error",
+    "jest/require-to-throw-message": "error",
+    "jest/valid-title": "error",
+	},
+};
+
+module.exports = eslintConfigForJest;
